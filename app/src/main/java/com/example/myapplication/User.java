126c7741405a889ab25c3db430c8c088a123e382
NguyenVanAnhTuann <anhlai.231002@gmail.com>
2022-11-24 11:35:40 +0700

    tuan

---
+++ app/src/main/java/com/example/myapplication/User.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+public class User {
+    public String email, pass;
+
+    public User() {
+
+    }
+
+    public User(String email, String pass) {
+        this.email = email;
+
+        this.pass = pass;
+
+
+    }
+}
+
+
+
